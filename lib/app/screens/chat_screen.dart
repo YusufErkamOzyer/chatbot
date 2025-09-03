5e34cbc2d15b6614f824e6994db8238de62f54c4
YusufErkamOzyer <yusuferkam.ozyer@gmail.com>
2025-09-03 14:31:07 +0300

    Implemented text message

---
+++ lib/app/screens/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:chatbot/core/components/text_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat Screen')),
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(color: Colors.grey[200]),
+          padding: EdgeInsets.all(16.0),
+          child: TextMessage(type: SenderType.home, time: "20:50"),
+        ),
+      ),
+    );
+  }
+}
