3455ab18c2006a05e1d3fd789e4f3328700b2271
YusufErkamOzyer <yusuferkam.ozyer@gmail.com>
2025-09-04 12:54:50 +0300

    removed unused dependency

--- lib/core/components/app_top_bar.dart
+++ lib/core/components/app_top_bar.dart
@@ -1,4 +1,3 @@
-import 'package:chatbot/core/color_constants.dart';
 import 'package:chatbot/core/widgets/chat_bot_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
