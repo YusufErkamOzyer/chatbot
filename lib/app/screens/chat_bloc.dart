ec3f44bb39da826ccdbdac480d33956293ecf8d6
YusufErkamOzyer <yusuferkam.ozyer@gmail.com>
2025-09-08 12:53:57 +0300

    Implemented BLoC statement to Chat Screen

---
+++ lib/app/screens/chat_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:chatbot/app/screens/chat_event.dart';
+import 'package:chatbot/app/screens/chat_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ChatBloc extends Bloc<ChatEvent, ChatState> {
+  final _firestore = FirebaseFirestore.instance;
+  Stream<List<Map<String, dynamic>>>? _messagesStream;
+  ChatBloc() : super(ChatInitial()) {
+    on<LoadMessages>(loadMessages);
+  }
+
+  Future<void> loadMessages(LoadMessages event, Emitter<ChatState> emit) async {
+    emit(ChatLoading());
+    try {
+      _messagesStream = _firestore
+          .collection('chats')
+          .doc('HDzEaDTOYUMPIXukoIbT')
+          .collection('messages')
+          .orderBy('timestamp', descending: true)
+          .snapshots()
+          .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
+      await emit.forEach<List<Map<String, dynamic>>>(
+        _messagesStream!,
+        onData: (messages) => ChatSuccess(messages),
+        onError: (e, _) => ChatError(e.toString()),
+      );
+    } catch (e) {
+      emit(ChatError(e.toString()));
+    }
+  }
+}
